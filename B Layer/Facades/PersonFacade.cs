0f92e148fabb9d8590f3565c9d069fa10d33ad08
Manh Hung Tran <manhhung.t94@gmail.com>
2016-05-25 13:14:41 +0200

    P layer

--- B Layer/Facades/PersonFacade.cs
+++ B Layer/Facades/PersonFacade.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+
 using B_Layer.DTO;
 using DA_Layer;
 using DA_Layer.Entities;
---
+++ P Layer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(P_Layer.Startup))]
+namespace P_Layer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
